56dbdb38f83f6b14141bc171c4df32aa0e50c082
Denilson Silva Lobo <denilson.lobo@totvs.com.br>
2023-06-28 21:22:53 -0300

    first commit

---
+++ UsersAPI.Services/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        /// <summary>
+        /// Criar conta de usuário
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Alterar dados de usuário
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Excluir conta de usuário
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Consultar os dados de usuário
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
