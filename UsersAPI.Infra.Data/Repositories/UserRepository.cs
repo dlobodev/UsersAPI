4f383f081c026c63f5a3f6dae5ef88a7f3b3c10e
Denilson Silva Lobo <denilson.lobo@totvs.com.br>
2023-06-30 22:00:24 -0300

    Design do domínio e repositório

---
+++ UsersAPI.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using UsersAPI.Domain.Interfaces.Repositories;
+using UsersAPI.Domain.Models;
+using UsersAPI.Infra.Data.Contexts;
+
+namespace UsersAPI.Infra.Data.Repositories
+{
+    public class UserRepository : BaseRepository<User, Guid>, IUserRepository
+    {
+        private readonly DataContext? _dataContext;
+
+        public UserRepository(DataContext? dataContext) : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
